0eb81a1cc4a8d64ec5d8f58bb7290ef431a08f11
Zlata Schastlivaya <s4astlivaya.zlata11@yandex.ru>
2025-11-14 15:06:06 +0300

    feat: solve task getIndexOf

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -265,8 +265,13 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let i = 0;
+  while (i < str.length && str[i] !== letter) {
+    i += 1;
+  }
+
+  return i < str.length ? i : -1;
 }
 
 /**
